17163dc9581722effac2505a9f203a5ac2fb56e4
David Escandón <david.escandon.v@gmail.com>
2021-12-10 13:24:42 -0300

    Update constructions.py

--- constructions.py
+++ constructions.py
@@ -22,3 +22,28 @@ pt.reflect("y")
 print((pt.x, pt.y))
 pt.y = 4.0
 print(pt.distance_to_origin())
+
+###ALTERNATIVE CONSTRUCTORS
+
+class Player:
+    MAX_POSITION = 10
+    
+    def __init__(self):
+        self.position = 0
+
+    # Add a move() method with steps parameter     
+    def move(self, steps):
+        if self.position + steps < Player.MAX_POSITION:
+           self.position = self.position + steps 
+        else:
+           self.position = Player.MAX_POSITION
+    
+    # This method provides a rudimentary visualization in the console    
+    def draw(self):
+        drawing = "-" * self.position + "|" +"-"*(Player.MAX_POSITION - self.position)
+        print(drawing)
+
+p = Player(); p.draw()
+p.move(4); p.draw()
+p.move(5); p.draw()
+p.move(3); p.draw()
