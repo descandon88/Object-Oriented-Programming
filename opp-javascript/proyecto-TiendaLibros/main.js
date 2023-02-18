8fa7c4eaaa51bfb475cdc7bcbff75408386ccdaf
David Escandón <david.escandon.v@gmail.com>
2023-02-18 20:33:49 -0300

    OOP: adding Comic class as an inheritance

--- opp-javascript/proyecto-TiendaLibros/main.js
+++ opp-javascript/proyecto-TiendaLibros/main.js
@@ -40,9 +40,11 @@ class Book {
     }
 }
 
-class Seller {
-    constructor(){
-
+class Comic extends Book {
+    constructor(pepe, autor, price, ilustradores, personajes){
+        super(pepe,autor,price);
+        this.ilustradores = ilustradores;
+        this.personajes=personajes;
     }
 }
 
@@ -51,23 +53,31 @@ class Seller {
 // Instancia de Book: 
 const libro01 = new Book("Los Tres Mosqueteros", "Alejandro Dumas", 320);
 const libro02 = new Book("Frankenstain", "G.O.", 350);
-
+const comic01 = new Comic("The Killing Joke", 'A.M.',150,['Brian Bolland','John Higgins'],['El Guasón','Batman', 'Barbara Gordon','Red Hood'])
 
 console.log(libro01);
 console.log('con el private.get: '+libro01.title);
 console.log(libro02.autor);
 
+console.log(comic01);
+
+console.log(comic01.titulo);
+console.log(comic01.price);
+
+console.log(comic01.ilustradores);
 
 
-class Book01 {
-    title="1984";
-    author="George Orwell";
-    price = 350;
-}
 
-const book01 = new Book01();
 
-console.log(book01);
-console.log(book01.title);
-console.log(book01.author);
-console.log(book01.price);
+// class Book01 {
+//     title="1984";
+//     author="George Orwell";
+//     price = 350;
+// }
+
+// const book01 = new Book01();
+
+// console.log(book01);
+// console.log(book01.title);
+// console.log(book01.author);
+// console.log(book01.price);
